f463daef6b6f8a964f6e24220b81e28a85c5efa6
Luan Ngo Minh <root@lulu>
2018-10-03 03:34:37 -0400

    Add go exporter

---
+++ go_exporter/cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	xxCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "page_counter",
+			Help: "Number of request access to xx page",
+		},
+		[]string{"page"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(xxCounter)
+}
+
+func main() {
+	http.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/xxx", func(w http.ResponseWriter, q *http.Request) {
+		xxCounter.With(prometheus.Labels{"page": "xx_page"}).Inc()
+		w.Write([]byte("meow meow meow"))
+	})
+	http.HandleFunc("/yyy", func(w http.ResponseWriter, q *http.Request) {
+		xxCounter.With(prometheus.Labels{"page": "yy_page"}).Inc()
+		w.Write([]byte("meow meow meow"))
+	})
+	log.Fatal(http.ListenAndServe(":9876", nil))
+}
